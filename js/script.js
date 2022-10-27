8b9128681e82e20eacdb10a600b60b8eae96b0db
Evgeniy Oshepkov <antarievg@yandex.ru>
2022-10-27 10:57:25 +0300

    ready lesson

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalCloseBtn = document.querySelector('.btn-close');
+const modalProductId = document.querySelector('.modal__id');
+const modalForm = document.querySelector('.form');
+const modalCheckbox = document.querySelector('.checkbox');
+const modalDiscontInput = document.querySelector('.js-discont-input');
+const modalTotalPrice = document.querySelector('.summary__count');
