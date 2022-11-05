3e4f2a41957f9daa49f3b0da3bc968b8d1cae9c6
Evgeniy Oshepkov <antarievg@yandex.ru>
2022-11-05 15:15:15 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
 'use strict';
 const goods = [
   {
-    'id': 246016548,
-    'title': 'Навигационная система Soundmax',
-    'category': 'Техника для дома',
-    'units': 'шт',
-    'count': 5,
-    'price': 100,
-    'total': 500,
+    id: 246016548,
+    title: 'Навигационная система Soundmax',
+    category: 'Техника для дома',
+    units: 'шт',
+    count: 5,
+    price: 100,
+    total: 500,
   },
   {
-    'id': 937295527,
-    'title': 'Настольная игра “На 4-х ногах”',
-    'category': 'Настольные игры',
-    'units': 'шт',
-    'count': 12,
-    'price': 14,
-    'total': 168,
+    id: 937295527,
+    title: 'Настольная игра “На 4-х ногах”',
+    category: 'Настольные игры',
+    units: 'шт',
+    count: 12,
+    price: 14,
+    total: 168,
   },
   {
-    'id': 253842678,
-    'title': 'Смартфон Xiaomi 11T 8/128GB',
-    'category': 'Смартфоны',
-    'units': 'шт',
-    'count': 4,
-    'price': 400,
-    'total': 1600,
+    id: 253842678,
+    title: 'Смартфон Xiaomi 11T 8/128GB',
+    category: 'Смартфоны',
+    units: 'шт',
+    count: 4,
+    price: 400,
+    total: 1600,
   },
   {
-    'id': 296378448,
-    'title': 'Радиоуправляемый автомобиль Cheetan',
-    'category': 'Игрушки',
-    'units': 'шт',
-    'count': 1,
-    'price': 60,
-    'total': 60,
+    id: 296378448,
+    title: 'Радиоуправляемый автомобиль Cheetan',
+    category: 'Игрушки',
+    units: 'шт',
+    count: 1,
+    price: 60,
+    total: 60,
   },
   {
-    'id': 216374586,
-    'title': 'Витая пара PROConnect 01-0043-3-25',
-    'category': 'Кабели',
-    'units': 'шт',
-    'count': 9,
-    'price': 10,
-    'total': 90,
+    id: 216374586,
+    title: 'Витая пара PROConnect 01-0043-3-25',
+    category: 'Кабели',
+    units: 'шт',
+    count: 9,
+    price: 10,
+    total: 90,
   },
 ];
 
@@ -97,10 +97,10 @@ tableBody.addEventListener('click', e => {
 
   if (target.closest('.delete')) {
     const row = target.closest('tr');
-    const targetId = row.dataset.id;
+    const targetId = +row.dataset.id;
 
     for (const i in goods) {
-      if (goods[i].id === +targetId) {
+      if (goods[i].id === targetId) {
         goods.splice(i, 1);
       }
     }
